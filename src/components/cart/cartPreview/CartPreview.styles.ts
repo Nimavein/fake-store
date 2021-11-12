9aee40ec17aabe1712c3ea6357dacd90a25cf30e
Nimavein <michalzygiel00@gmail.com>
2021-11-12 18:59:33 +0100

    add box shadow on cart preview

--- src/components/cart/cartPreview/CartPreview.styles.ts
+++ src/components/cart/cartPreview/CartPreview.styles.ts
@@ -17,6 +17,10 @@ export const CartPreviewWrapper = styled.section<{ isCartOpen: boolean }>`
   text-align: center;
   border-radius: 0 0 0 16px;
   overflow: scroll;
+  box-shadow: 3.38443px 55.8976px 80px rgba(97, 121, 139, 0.07),
+    1.71337px 28.2982px 34.875px rgba(97, 121, 139, 0.04725),
+    0.676885px 11.1795px 13px rgba(97, 121, 139, 0.035),
+    0.148069px 2.44552px 4.625px rgba(97, 121, 139, 0.02275);
 
   &::-webkit-scrollbar {
     display: none;
